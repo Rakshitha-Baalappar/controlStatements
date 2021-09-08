a787ec8310994a2103ffa5666e7fe6c0d1ab88b0
Rakshitha-Baalappar <90265131+Rakshitha-Baalappar@users.noreply.github.com>
2021-09-08 11:53:18 +0530

    Add files via upload

---
+++ DisplayNaturalNumbers.java
@@ -0,0 +1,16 @@
+package com.xworkz.controlStatements;
+
+public class DisplayNaturalNumbers {
+
+	public static void main(String[] args) {
+		byte i=1;
+		System.out.println("List of first 10 natural numbers \n");
+		do {
+			
+			System.out.println(i);
+			i++;
+		}while(i<=10);
+
+	}
+
+}
---
+++ NaturalNumbers.java
@@ -0,0 +1,13 @@
+package com.xworkz.controlStatements;
+
+public class NaturalNumbers {
+
+	public static void main(String[] args) {
+		byte num=1;
+		System.out.println("List of natural numbers");
+		while(num<=10) {
+			System.out.println(num);
+			num++;
+		}
+	}
+}
